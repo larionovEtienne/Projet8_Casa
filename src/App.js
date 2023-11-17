a4ba361616cd1803de9e3ee0531c2e041fec917e
larionovEtienne <145725693+larionovEtienne@users.noreply.github.com>
2023-11-17 12:11:53 +0100

    version80%

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import MainContainer from "./components/MainContainer";
+import Footer from "./components/Footer";
+import PropertyDetail from "./components/PropertyDetail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/property/:id" element={<PropertyDetailHeaderOnly />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
+const Home = () => (
+  <>
+    <Header />
+    <MainContainer />
+    <Footer />
+  </>
+);
+
+const PropertyDetailHeaderOnly = () => (
+  <>
+    <Header />
+    <PropertyDetail />
+  </>
+);
 
 export default App;
